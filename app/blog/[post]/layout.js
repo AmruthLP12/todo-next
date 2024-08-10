21de29171ad642129be7c887ce77951ba96acc32
Amruth L P <134767358+AmruthLP12@users.noreply.github.com>
2024-08-10 19:38:31 +0530

    Dynamic Routing in Next.js

--- app/blog/[post]/layout.js
+++ app/blog/[post]/layout.js
@@ -2,3 +2,10 @@ const BlogLayout = ({children}) => {
     return children;
 }
 
+export default BlogLayout;
+
+export async function generateMetadata({params}) {
+    return{
+        title:params.post.split('-').join(" ")
+    }
+}
--- app/blog/[post]/page.js
+++ app/blog/[post]/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = ({params}) => {
   return (
-    <div>page {params.post}</div>
+    <div>Post : {params.post}</div>
   )
 }
 
