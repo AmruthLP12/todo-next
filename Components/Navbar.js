b37ebe670efbc186bc5837a637d649fd3494fbbd
Amruth L P <134767358+AmruthLP12@users.noreply.github.com>
2024-08-11 12:38:12 +0530

    sdsd

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='flex py-3 flex-warp justify-around text-white'>
-        <h1 className='text-lg font-semibold' >Todo App</h1>
+        <h1 className='text-lg font-semibold' >Todo Next App</h1>
         <ul className='flex gap-[40px] text-m'>
             <li>Home</li>
             <li>Products</li>
